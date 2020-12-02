ad1057dd0d6a63f254f46cb1027f5340d80598eb
Fabricio Garcia <gael.fabricio@gmail.com>
2020-12-01 19:52:06 -0600

    feat: Add Landing component
    - Create folders component and add Landing
    - Create fonts folder and add facebook font

---
+++ src/components/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="col-md-6">
+          <h1 className="title">facebook</h1>
+        </div>
+        <div className="col-md-6">
+          <h1>Registration Form</h1>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Landing;
