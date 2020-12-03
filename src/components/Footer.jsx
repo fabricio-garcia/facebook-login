bfbbf8705817c14179568e7a2312485de4e07245
Fabricio Garcia <gael.fabricio@gmail.com>
2020-12-03 15:58:47 -0600

    feat: Finish Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className="container mb-5">
+      <div className="row top-lang-row">
+        <div>
+          <ul className="list-group list-group-horizontal languages">
+            <li>English (US)</li>
+            <li>Español</li>
+            <li>Portugués (Brasil)</li>
+            <li>Francais (France)</li>
+            <li>Deutch</li>
+            <li>Italiano</li>
+            <li>日本語</li>
+            <li>العربية</li>
+            <li>हिन्दी</li>
+            <li>中文(简体)</li>
+          </ul>
+        </div>
+      </div>
+      <div className="row">
+        <div>
+          <small style={{ color: '#94979E' }}>Facebook © 2020</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,7 @@ import Registration from './Registration';
 const Landing = () => {
   return (
     <React.Fragment>
-      <div className="row align-items-center landing-container flex-grow-1">
+      <div className="row mb-4 align-items-center landing-container flex-grow-1">
         <div className="col-md-2"></div>
         <div className="col-md-4 text-left">
           <h1 className="fb-font title">facebook</h1>
